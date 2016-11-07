3fe72378da534e51293df3fe19f24a911a537402
Lucas Pereira da Silva <lucas@dominiol.com.br>
2016-11-07 15:02:56 -0200

    Commit message hook.

--- commit-msg.py
+++ commit-msg.py
@@ -7,10 +7,8 @@ message = sys.argv[0]
 pattern = re.compile('^([A-Z]+) (#[0-9]+) - ([A-Z][a-zA-Z ]+)$')
 test = pattern.match(message)
 
-print(message)
 if test == None:
 	print('Invalid commit message')
 	sys.exit(1)
 else:
-	print('Valid commit message')
 	sys.exit(0)
