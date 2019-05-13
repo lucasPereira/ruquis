06b52dbe166b142482352dc8979b5f09b6a6508f
Lucas Pereira da Silva <lucas@dominiol.com.br>
2019-05-13 16:06:06 -0300

    Permite números na mensagem de commit

--- commit-msg-simple-text.py
+++ commit-msg-simple-text.py
@@ -6,7 +6,7 @@ import sys
 
 messageFile = sys.argv[1]
 message = open(messageFile, 'r').read()
-pattern = re.compile('^([A-ZÁ]([ ]?[a-zA-ZãÃáÁàÀõÕóÓéÉíÍúÚçÇ]+)*)$', re.MULTILINE|re.UNICODE|re.LOCALE)
+pattern = re.compile('^([A-ZÁ]([ ]?[0-9a-zA-ZãÃáÁàÀõÕóÓéÉíÍúÚçÇ]+)*)$', re.MULTILINE|re.UNICODE|re.LOCALE)
 test = pattern.match(message);
 if test == None or len(test.groups()) == 0:
 	print('Mensagem de commit inválida. A mensagem deve possuir apenas letras e deve começar com letra maiúscula.')
